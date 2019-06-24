5302544c6f352b06917366ba0dacf53339e446c0
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-24 18:56:21 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,42 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/articles' do
+    @articles = Article.all
+    erb :index
   end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  post '/articles' do
+    @article = Article.create(params)
+    redirect to("/articles/#{@article.id}")
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    puts params, "hey"
+    @article = Article.find(params[:id])
+    @article.update(title: params[:title], content: params[:content])
+    redirect to("/articles/#{params[:id]}")
+  end
+
+  delete '/articles/:id' do
+    puts params, "hey"
+    Article.find(params[:id]).delete
+    binding.pry
+    redirect to("/articles")
+  end
+
 end
